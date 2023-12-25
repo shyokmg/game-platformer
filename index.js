02935db764e2c282bb6ece87534c14ee3b954c15
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-12-25 13:38:06 +1100

    Added gravity

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const gravity = 0.5;
+
+
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 0
         }
         this.width = 30;
         this.height = 30;
@@ -23,7 +31,26 @@ class Player {
             this.height
         );
     }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+    }
 }
 
 const player = new Player()
-player.draw();
+// player.draw();
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+}
+
+animate();
