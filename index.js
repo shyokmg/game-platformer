4093f75ee189416c67f63a8970fd8f771c8857ee
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-12-25 13:23:48 +1100

    Created initial player

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ class Player {
             x: 100,
             y: 100
         }
-        this.width = 100;
-        this.height = 100;
+        this.width = 30;
+        this.height = 30;
     }
 
     draw() {
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.fillStyle = 'red';
+        c.fillRect(
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+        );
     }
 }
 
