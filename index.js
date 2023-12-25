295137af2ce1aea3249d6eccffa6c1a457cdb0fb
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-12-25 15:16:42 +1100

    Added platform and scrolled background

--- index.js
+++ index.js
@@ -45,7 +45,39 @@ class Player {
     }
 }
 
+class Platform {
+    constructor({ x, y }) {
+        this.position = {
+            x, // same as x : x
+            y  // same as y : y
+        };
+        this.width = 200;
+        this.height = 20;
+    }
+
+    draw() {
+        c.fillStyle = 'blue';
+        c.fillRect(
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+    }
+}
+
 const player = new Player()
+const platforms = [
+    new Platform({
+        x: 200,
+        y: 100
+    }), 
+    new Platform({
+        x: 500,
+        y: 200
+    })
+]
+
 const keys = {
     right: {
         pressed: false
@@ -56,16 +88,45 @@ const keys = {
 }
 
 function animate() {
+    // recall function
     requestAnimationFrame(animate)
+    // removes draw from prev postion
     c.clearRect(0, 0, canvas.width, canvas.height);
+    // update player position
     player.update();
-    if (keys.right.pressed) {
-        player.velocity.x = 5;
-    } else if (keys.left.pressed) {
-        player.velocity.x = -5;
-    } else  player.velocity.x = 0;
 
-    
+    // draw platform
+    platforms.forEach(platform => {
+        platform.draw();
+    })
+
+    // If right or left keys are pressed move right or left in 5px
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5;
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5;
+    } else {
+        player.velocity.x = 0;
+
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x -= 5;
+            })
+        } else if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x += 5;
+            })
+        }
+    }
+    platforms.forEach(platform => {
+        // platfrom collision detection
+        if (player.position.y + player.height <= platform.position.y
+            && player.position.y + player.height + player.velocity.y >= platform.position.y
+            && player.position.x + player.width >= platform.position.x
+            && player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0;
+        }
+    });
 }
 
 animate();
@@ -85,7 +146,7 @@ addEventListener('keydown', ({ keyCode }) => {
             break;
         // up key: W
         case 87:
-            player.velocity.y -= 10
+            player.velocity.y -= 20
             break;
     }
 });
@@ -105,7 +166,7 @@ addEventListener('keyup', ({ keyCode }) => {
             break;
         // up key: W
         case 87:
-            player.velocity.y -= 10
+            player.velocity.y = 0
             break;
     }
 });
