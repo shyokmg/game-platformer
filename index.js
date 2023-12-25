831db5b7316da4bd3c2cf2c0e210c6c1f5909077
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-12-25 14:28:35 +1100

    Added basic player movement

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ class Player {
     draw() {
         c.fillStyle = 'red';
         c.fillRect(
-            this.position.x, 
-            this.position.y, 
-            this.width, 
+            this.position.x,
+            this.position.y,
+            this.width,
             this.height
         );
     }
@@ -35,6 +35,7 @@ class Player {
     update() {
         this.draw();
         this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height) {
             this.velocity.y += gravity;
@@ -45,12 +46,66 @@ class Player {
 }
 
 const player = new Player()
-// player.draw();
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+    if (keys.right.pressed) {
+        player.velocity.x = 5;
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5;
+    } else  player.velocity.x = 0;
+
+    
 }
 
 animate();
+
+addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        // left key: A
+        case 65:
+            keys.left.pressed = true;
+            break;
+        // down key: S
+        case 83:
+            break;
+        // right key: D
+        case 68:
+            keys.right.pressed = true;
+            break;
+        // up key: W
+        case 87:
+            player.velocity.y -= 10
+            break;
+    }
+});
+
+addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        // left key: A
+        case 65:
+            keys.left.pressed = false;
+            break;
+        // down key: S
+        case 83:
+            break;
+        // right key: D
+        case 68:
+            keys.right.pressed = false;
+            break;
+        // up key: W
+        case 87:
+            player.velocity.y -= 10
+            break;
+    }
+});
